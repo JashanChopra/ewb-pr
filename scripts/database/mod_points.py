7c881b7ea8e3314de23357e2a016abe1b8b7d9c9
Jashan <jashanxchopra@gmail.com>
2019-08-04 17:43:39 -0600

    updated appending to database

--- scripts/database/mod_points.py
+++ scripts/database/mod_points.py
@@ -1,5 +1,5 @@
 # import libraries
-from database.gpxfuncs import loadgpx
+from database.gpxfuncs import loadgpx, loadgpxfiles
 from database.databaseFuncs import create_poi, get_conn
 
 
@@ -10,8 +10,9 @@ def append_names():
     """
 
     # ask the user to enter filename, repeat until correct
-    while True:
-        filename = input('Enter the filename of the GPX file you want to add to the database: ')
+
+    newfiles = loadgpxfiles()
+    for filename in newfiles:
         try:
             gpx = loadgpx(filename)
             print(f'{filename} read properly')
@@ -21,25 +22,25 @@ def append_names():
             print(f'{filename} not found in directory, try again')
             pass
 
-    for waypoint in gpx.waypoints:
-        namechange = input(f'Do you want to change the name of waypoint {waypoint.name}? [y/n]: ')
+        for waypoint in gpx.waypoints:
+            namechange = input(f'Do you want to change the name of waypoint {waypoint.name}? [y/n]: ')
 
-        if namechange == 'y':
-            waypoint.name = input('Enter the new name of the waypoint: ')
-            print(f'The new name of this waypoint is {waypoint.name}')
-            print('*------------*')
+            if namechange == 'y':
+                waypoint.name = input('Enter the new name of the waypoint: ')
+                print(f'The new name of this waypoint is {waypoint.name}')
+                print('*------------*')
 
-        else:
-            print(f'The waypoint name will stay as {waypoint.name}')
-            print('*------------*')
+            else:
+                print(f'The waypoint name will stay as {waypoint.name}')
+                print('*------------*')
 
-        addpoint = (waypoint.name, waypoint.latitude,
-                    waypoint.longitude, waypoint.elevation,
-                    waypoint.time)
+            addpoint = (waypoint.name, waypoint.latitude,
+                        waypoint.longitude, waypoint.elevation,
+                        waypoint.time)
 
-        conn = get_conn(r'C:\Users\Jashan\PycharmProjects\ewb-pr\data\gpspoints.db')
-        idx = create_poi(conn, addpoint)
-        print(f'{waypoint.name} has been added to the database with id {idx}')
+            conn = get_conn(r'C:\Users\Jashan\PycharmProjects\ewb-pr\data\gpspoints.db')
+            idx = create_poi(conn, addpoint)
+            print(f'{waypoint.name} has been added to the database with id {idx}')
 
 
 if __name__ == '__main__':
